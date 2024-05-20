7b6d6622e3f8878b58b968d72ce80c1569f68463
Bishawjit-Mondol <bishawjitmondol@gmail.com>
2024-05-20 15:09:07 +0600

    crud using backpack model (Category)

---
+++ resources/views/vendor/backpack/ui/dashboard.blade.php
@@ -0,0 +1,54 @@
+@extends(backpack_view('layouts.top_left'))
+
+@php
+
+    @endphp
+
+
+@section('content')
+    <div class="row">
+
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-header">
+                    Welcome to the Dashboard
+                </div>
+                <div class="card-body">
+                    <p>
+                        Laravel Backpack is a set of packages that can be used to build custom admin panels for any web
+                        application. It's a great way to build admin panels, CRUDs, and dashboards.
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-header">
+                    Welcome to the Dashboard
+                </div>
+                <div class="card-body">
+                    <p>
+                        Laravel Backpack is a set of packages that can be used to build custom admin panels for any web
+                        application. It's a great way to build admin panels, CRUDs, and dashboards.
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-header">
+                    Welcome to the Dashboard
+                </div>
+                <div class="card-body">
+                    <p>
+                        Laravel Backpack is a set of packages that can be used to build custom admin panels for any web
+                        application. It's a great way to build admin panels, CRUDs, and dashboards.
+                    </p>
+                </div>
+            </div>
+        </div>
+
+    </div>
+@endsection
